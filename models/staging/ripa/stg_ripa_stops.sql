3bc2723d72f6a49bd8b446e40fc923e235ea9184
Alan Ward <alan.ward999@gmail.com>
2025-07-26 13:36:20 -0700

    Add cleaned staging model for ripa_stops

--- models/staging/ripa/stg_ripa_stops.sql
+++ models/staging/ripa/stg_ripa_stops.sql
@@ -1,10 +1,52 @@
 {{ config(
-    materialized='view',
-    enabled=true
+    materialized = 'view'
 ) }}
 
--- Minimal working model
+with source as (
 
-select
-    1 as id,
-    'test' as note
+    select * 
+    from {{ source('raw', 'ripa_stops_datasd') }}
+
+),
+
+renamed as (
+
+    select
+        id as stop_id,
+        ori,
+        agency,
+        expYears as years_experience,
+        stopdate as stop_date,
+        stoptime as stop_time,
+        stopduration as stop_duration_minutes,
+        stopInResponseToCFS as stop_in_response_to_cfs,
+        stopmadeduringwelfarecheck as stop_during_welfare_check,
+        stoptype,
+        officerAssignmentkey as officer_assignment_key,
+        assignment,
+        intersection,
+        block,
+        landmark,
+        street,
+        highwayExit as highway_exit,
+        isschool as is_school,
+        schoolName as school_name,
+        cityname as city_name,
+        beat,
+        beatName as beat_name,
+        PID as person_id,
+        isstudent as is_student,
+        perceivedLimitedEnglish as limited_english_flag,
+        perceivedAge as perceived_age,
+        perceivedGender as perceived_gender,
+        NonBinaryPerson as non_binary_flag,
+        perceivedUnhoused as unhoused_flag,
+        perceivedSexualOrientation as perceived_sexual_orientation,
+        passengerInVehicle as passenger_flag,
+        insideResidence as inside_residence_flag
+    from source
+
+)
+
+select * 
+from renamed
