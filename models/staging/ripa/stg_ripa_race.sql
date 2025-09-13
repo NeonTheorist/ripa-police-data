175530f206b5c312faa8cc4fb0f6801b88b1efc5
Alan Ward <alan.ward999@gmail.com>
2025-09-13 15:51:13 -0700

    Clean race categories in stg_ripa_race (collapse Hispanic variants)

--- models/staging/ripa/stg_ripa_race.sql
+++ models/staging/ripa/stg_ripa_race.sql
@@ -15,7 +15,11 @@ renamed as (
         id as stop_id,
         --regexp_replace(cast(id as string), '[^0-9]', '') as stop_id,
         pid as person_id,
-        race as perceived_race,
+        case
+            when race in ('Hispanic/Latine(x)', 'Hispanic/Latino(a)')
+                then 'Hispanic/Latino(a)'
+            else race
+        end as perceived_race,
         insertdatetime as inserted_at
     from source
 
